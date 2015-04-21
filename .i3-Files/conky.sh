61899e426ab57f6161f6dfe5cd874a7d68948d02
Erik Carlson <carlsonerikw@gmail.com>
2015-04-21 18:14:50 -0400

    All i3 and Xresource files

---
+++ .i3-Files/conky.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+echo "{\"version\":1}"
+echo "[[] ,"
+#conky -c ~/.i3/ConkyScripts/conkyrc-molokai
+
+#conky -c ~/.i3/ConkyScripts/conkyrc-base16-twilight-light
+conky -c ~/.i3/ConkyScripts/conkyrc-base16-twilight-dark
+#conky -c ~/.i3/ConkyScripts/conkyrc-solarized-dark
+#conky -c ~/.i3/ConkyScripts/conkyrc-solarized-light
