a848cfeb342ce077883daa4b6f26d3fdc8f88078
Erik Carlson <carlsonerikw@gmail.com>
2016-07-19 23:23:27 -0400

    Move to i3blocks for statusline instead of Conky

--- i3-install/.i3-Files/conky.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-echo "{\"version\":1}"
-echo "[[] ,"
-#conky -c ~/.i3/ConkyScripts/conkyrc-molokai
-
-#conky -c ~/.i3/ConkyScripts/conkyrc-base16-twilight-light
-conky -c ~/.i3/ConkyScripts/conkyrc-base16-twilight-dark
-#conky -c ~/.i3/ConkyScripts/conkyrc-solarized-dark
-#conky -c ~/.i3/ConkyScripts/conkyrc-solarized-light
